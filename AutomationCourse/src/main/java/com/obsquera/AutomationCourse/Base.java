394e83ef33db2d6ce2771d341d9ab321fad56a97
user <user@DESKTOP-LEE528R.bbrouter>
2023-05-15 08:43:12 +0530

    Commit on May 15

---
+++ AutomationCourse/src/main/java/com/obsquera/AutomationCourse/Base.java
@@ -0,0 +1,25 @@
+package com.obsquera.AutomationCourse;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	public WebDriver driver;
+
+	
+	public void initializeBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Documents\\Seleniumtestingtutorial\\AutomationCourse\\src\\main\\java\\Resources\\chromedriver.exe" );
+		driver=new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+	}
+	
+
+	public static void main(String[] args) {
+		
+		Base base=new Base();
+		base.initializeBrowser();
+
+	}
+
+}
