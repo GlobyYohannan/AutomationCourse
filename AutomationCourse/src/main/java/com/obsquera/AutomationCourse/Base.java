17e437258cbaf6e09dd96231d449dba2d280cf3a
user <user@DESKTOP-LEE528R.bbrouter>
2023-05-16 07:09:00 +0530

    3rd commit may 15

--- AutomationCourse/src/main/java/com/obsquera/AutomationCourse/Base.java
+++ AutomationCourse/src/main/java/com/obsquera/AutomationCourse/Base.java
@@ -1,8 +1,10 @@
 package com.obsquera.AutomationCourse;
 
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Base {
 	public WebDriver driver;
@@ -11,12 +13,17 @@ public class Base {
 	public void initializeBrowser()
 	{
 		//To invoke Chrome Browser
-		//System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Documents\\Seleniumtestingtutorial\\AutomationCourse\\src\\main\\java\\Resources\\chromedriver.exe" );
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\git\\AutomationCourse\\AutomationCourse\\src\\main\\java\\Resources\\chromedriver.exe" );
 		//driver=new ChromeDriver();
 		
 		//To invoke Edge Browser
-		System.setProperty("webdriver.edge.driver","C:\\Users\\user\\git\\AutomationCourse\\AutomationCourse\\src\\main\\java\\Resources\\msedgedriver.exe" );
-		driver=new EdgeDriver();
+		//System.setProperty("webdriver.edge.driver","C:\\Users\\user\\git\\AutomationCourse\\AutomationCourse\\src\\main\\java\\Resources\\msedgedriver.exe" );
+		//driver=new EdgeDriver();
+		//driver.get("https://www.amazon.in/");
+		
+		//To invoke firefox Browser
+		//System.setProperty("webdriver.gecko.driver","C:\\Users\\user\\Documents\\Seleniumtestingtutorial\\AutomationCourse2\\src\\main\\java\\Resources\\geckodriver.exe" );
+		//driver= new FirefoxDriver();
 		driver.get("https://www.amazon.in/");
 	}
 	
